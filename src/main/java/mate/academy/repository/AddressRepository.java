fbbdf850dfec8655152f79d95a4f69eb64b2fe6a
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-11-30 21:19:38 +0200

    Created AddressRepository.

---
+++ src/main/java/mate/academy/repository/AddressRepository.java
@@ -0,0 +1,10 @@
+package mate.academy.repository;
+
+import mate.academy.model.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddressRepository extends JpaRepository<Address, Long> {
+    boolean existsByCountryAndCityAndStateAndStreetAndHouseNumber(
+            String country, String city, String state, String street, String houseNumber
+    );
+}
