e1ed33454aea7ffe9d918e23af947ec0d09bd8a4
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-11-30 22:01:08 +0200

    Removed unnecessary classes

--- src/main/java/mate/academy/service/AuthenticationService.java
+++
@@ -1,8 +0,0 @@
-package mate.academy.service;
-
-import mate.academy.dto.user.UserLoginRequestDto;
-import mate.academy.dto.user.UserLoginResponseDto;
-
-public interface AuthenticationService {
-    UserLoginResponseDto authenticate(UserLoginRequestDto requestDto);
-}
