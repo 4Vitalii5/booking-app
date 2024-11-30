ca8618418f10b3545fdbb9b4143ffa8479d2fb8f
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-11-30 21:19:09 +0200

    Created Addressdto.

---
+++ src/main/java/mate/academy/dto/address/AddressDto.java
@@ -0,0 +1,12 @@
+package mate.academy.dto.address;
+
+public record AddressDto(
+        Long id,
+        String country,
+        String city,
+        String state,
+        String street,
+        String houseNumber,
+        String postalCode
+) {
+}
