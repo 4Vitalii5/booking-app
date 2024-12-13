d8286ddc68d52411e9e03bc92152b1b2df9508e3
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-12-13 12:33:48 +0200

    Removed soft delete from entity address

--- src/main/java/mate/academy/model/Address.java
+++ src/main/java/mate/academy/model/Address.java
@@ -6,17 +6,16 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Getter
 @Setter
-@SQLDelete(sql = "UPDATE addresses SET is_deleted = true WHERE id=?")
-@SQLRestriction("is_deleted=false")
-@Table(name = "addresses")
+@Table(name = "addresses", uniqueConstraints = {@UniqueConstraint(
+        columnNames = {"country", "city", "state", "street", "house_number", "postal_code"})
+})
 public class Address {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -33,7 +32,4 @@ public class Address {
     private String houseNumber;
     @Column(nullable = false)
     private String postalCode;
-    @Column(nullable = false)
-    private boolean isDeleted = false;
 }
-
