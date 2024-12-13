4b9f327390607ee95c2013eccdd94eb8e67cd8c5
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-12-13 12:47:51 +0200

    Created payment mapper

---
+++ src/main/java/mate/academy/mapper/PaymentMapper.java
@@ -0,0 +1,18 @@
+package mate.academy.mapper;
+
+import mate.academy.config.MapperConfig;
+import mate.academy.dto.payment.CreatePaymentRequestDto;
+import mate.academy.dto.payment.PaymentDto;
+import mate.academy.model.Payment;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(config = MapperConfig.class)
+public interface PaymentMapper {
+    @Mapping(source = "amountToPay", target = "amount")
+    PaymentDto toDto(Payment payment);
+
+    @Mapping(source = "bookingId", target = "booking.id")
+    @Mapping(source = "amount", target = "amountToPay")
+    Payment toEntity(CreatePaymentRequestDto requestDto);
+}
