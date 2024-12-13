f57ccb6ff3202819588e66e9df47b2a636a5ff97
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-12-13 13:04:31 +0200

    Added UserRepository

--- src/main/java/mate/academy/repository/UserRepository.java
+++ src/main/java/mate/academy/repository/UserRepository.java
@@ -7,5 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     @EntityGraph(attributePaths = {"roles"})
+    Optional<User> findById(Long id);
+
+    @EntityGraph(attributePaths = {"roles"})
     Optional<User> findByEmail(String email);
 }
