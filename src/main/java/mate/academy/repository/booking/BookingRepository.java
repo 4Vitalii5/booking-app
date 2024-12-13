58a2ed85f1a476f1f6ca5031bdf67f9419203dff
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-12-13 22:01:49 +0200

    Added findAllByUserId for payment service logic

--- src/main/java/mate/academy/repository/booking/BookingRepository.java
+++ src/main/java/mate/academy/repository/booking/BookingRepository.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import mate.academy.model.Booking;
+import mate.academy.model.Payment;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.EntityGraph;
@@ -17,7 +18,11 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
             LocalDate checkInDate, LocalDate checkOutDate, Long accommodationId
     );
 
+    @EntityGraph(attributePaths = {"user"})
+    List<Booking> findAllByUserId(Long userId);
+
     List<Booking> findByUserId(Long id, Pageable pageable);
 
+    @EntityGraph(attributePaths = {"user"})
     List<Booking> findAll(Specification<Booking> bookingSpecification, Pageable pageable);
 }
