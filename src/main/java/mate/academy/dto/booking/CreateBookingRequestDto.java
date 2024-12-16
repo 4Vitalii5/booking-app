62c0768a8af1e1accdfef12edfe07bcd82915238
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-12-16 14:00:10 +0200

    Added date checks to correctly create reservations.

--- src/main/java/mate/academy/dto/booking/CreateBookingRequestDto.java
+++ src/main/java/mate/academy/dto/booking/CreateBookingRequestDto.java
@@ -1,10 +1,22 @@
 package mate.academy.dto.booking;
 
+import jakarta.validation.constraints.Min;
 import java.time.LocalDate;
+import mate.academy.validation.FieldsDatesValid;
+import mate.academy.validation.FutureOrPresent;
 
+@FieldsDatesValid.List({
+        @FieldsDatesValid(
+                field = "checkInDate",
+                fieldMatch = "checkOutDate",
+                message = "CheckOutDate can't be before or equal checkInDate!"
+        )
+})
 public record CreateBookingRequestDto(
+        @FutureOrPresent
         LocalDate checkInDate,
         LocalDate checkOutDate,
+        @Min(1)
         Long accommodationId
 ) {
 }
