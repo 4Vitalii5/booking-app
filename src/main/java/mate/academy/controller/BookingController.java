038fc3463a98e5e69c277671b7ac292d69f82f5a
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-12-17 17:44:43 +0200

    Deleted comments

--- src/main/java/mate/academy/controller/BookingController.java
+++ src/main/java/mate/academy/controller/BookingController.java
@@ -88,12 +88,3 @@ public class BookingController {
         bookingService.deleteById(user, id);
     }
 }
-//Booking Controller: Managing users' bookings
-//
-//POST: /bookings - Permits the creation of new accommodation bookings.
-//GET: /bookings/?user_id=...&status=... -
-// Retrieves bookings based on user ID and their status. (Available for managers)
-//GET: /bookings/my - Retrieves user bookings
-//GET: /bookings/{id} - Provides information about a specific booking.
-//PUT/PATCH: /bookings/{id} - Allows users to update their booking details.
-//        DELETE: /bookings/{id} - Enables the cancellation of bookings.
