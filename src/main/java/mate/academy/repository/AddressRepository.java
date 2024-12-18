3c7e102ae3214e0a95e782f73823e40f0df3896a
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-12-18 20:10:06 +0200

    Added method findByCountryAndCityAndStateAndStreetAndHouseNumber to AddressRepository

--- src/main/java/mate/academy/repository/AddressRepository.java
+++ src/main/java/mate/academy/repository/AddressRepository.java
@@ -7,4 +7,8 @@ public interface AddressRepository extends JpaRepository<Address, Long> {
     boolean existsByCountryAndCityAndStateAndStreetAndHouseNumber(
             String country, String city, String state, String street, String houseNumber
     );
+
+    Address findByCountryAndCityAndStateAndStreetAndHouseNumber(
+            String country, String city, String state, String street, String houseNumber
+    );
 }
