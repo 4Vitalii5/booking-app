4303253bfe5a8d26c513ca4ed10257c562bd66a1
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-12-20 19:29:35 +0200

    Changed package name form mate.academy to org.cyberrealm.tech. Added @EnableScheduling and @EnableAsync

--- src/main/java/org/cyberrealm/tech/BookingAppApplication.java
+++ src/main/java/org/cyberrealm/tech/BookingAppApplication.java
@@ -1,9 +1,13 @@
-package mate.academy;
+package org.cyberrealm.tech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class BookingAppApplication {
 
     public static void main(String[] args) {
