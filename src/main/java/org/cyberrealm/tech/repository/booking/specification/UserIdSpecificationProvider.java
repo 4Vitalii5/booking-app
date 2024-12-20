c8dafbed14b628b3fa9729185e8e3a00a111a400
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2024-12-20 19:26:51 +0200

    Changed package name form mate.academy to org.cyberrealm.tech. Made constants public.

--- src/main/java/org/cyberrealm/tech/repository/booking/specification/UserIdSpecificationProvider.java
+++ src/main/java/org/cyberrealm/tech/repository/booking/specification/UserIdSpecificationProvider.java
@@ -1,16 +1,16 @@
-package mate.academy.repository.booking.specification;
+package org.cyberrealm.tech.repository.booking.specification;
 
 import java.util.Arrays;
-import mate.academy.model.Booking;
-import mate.academy.repository.SpecificationProvider;
+import org.cyberrealm.tech.model.Booking;
+import org.cyberrealm.tech.repository.SpecificationProvider;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserIdSpecificationProvider implements SpecificationProvider<Booking> {
-    private static final String USER_FIELD = "user";
-    private static final String ID_FIELD = "id";
-    private static final String USER_ID_FIELD = "userId";
+    public static final String USER_FIELD = "user";
+    public static final String ID_FIELD = "id";
+    public static final String USER_ID_FIELD = "userId";
 
     @Override
     public String getKey() {
