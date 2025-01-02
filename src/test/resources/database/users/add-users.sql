bf71c158ef907d9101fedfd15a87ff34649a2be7
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-01-02 20:35:17 +0200

    Added add-users.sql script

---
+++ src/test/resources/database/users/add-users.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (id, email, first_name, last_name,  password, is_deleted) VALUES
+(1, 'test@example.com', 'John', 'Doe', '$2a$10$E.bGzm/J4pvG8n84y5Y/eeJ9kA5sWy/h5ojNCdozPQshRKx/A2YkK', false),
+(2, 'maks@example.com', 'Maks', 'Kryskiv', '$2a$12$NWIYhIQUnpt9uHwdtA/RK.E7XMIgwmPgjJLMArJnQBK/qAiqy.Eq6', false);
