b41430576b3124b874efd7d4226d6ec0591c79c3
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-01-07 20:15:28 +0200

    Added sql script for cleaning roles table before test class

--- src/test/java/org/cyberrealm/tech/controller/AuthenticationControllerTest.java
+++ src/test/java/org/cyberrealm/tech/controller/AuthenticationControllerTest.java
@@ -23,6 +23,8 @@ import org.springframework.test.web.servlet.MvcResult;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@Sql(scripts = "classpath:database/roles/remove-roles.sql",
+        executionPhase = Sql.ExecutionPhase.BEFORE_TEST_CLASS)
 public class AuthenticationControllerTest {
     @Autowired
     private MockMvc mockMvc;
