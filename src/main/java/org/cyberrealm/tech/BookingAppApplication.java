5b4bc70bdd28b55da39ab45a3b40207353b6f285
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-01-09 23:30:25 +0200

    Enabled feign clients

--- src/main/java/org/cyberrealm/tech/BookingAppApplication.java
+++ src/main/java/org/cyberrealm/tech/BookingAppApplication.java
@@ -2,12 +2,14 @@ package org.cyberrealm.tech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableAsync
+@EnableFeignClients
 public class BookingAppApplication {
 
     public static void main(String[] args) {
