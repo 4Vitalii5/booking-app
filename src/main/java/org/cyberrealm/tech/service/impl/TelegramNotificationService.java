311f45eda2923d9b8d908081c0cb54b5c6a300cc
Vitalii Pavlyk <pavlykvitalii@gmail.com>
2025-01-09 23:31:42 +0200

    Changed webClient to telegramClient

--- src/main/java/org/cyberrealm/tech/service/impl/TelegramNotificationService.java
+++ src/main/java/org/cyberrealm/tech/service/impl/TelegramNotificationService.java
@@ -1,33 +1,22 @@
 package org.cyberrealm.tech.service.impl;
 
-import jakarta.annotation.PostConstruct;
 import org.cyberrealm.tech.service.NotificationService;
+import org.cyberrealm.tech.service.TelegramClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 public class TelegramNotificationService implements NotificationService {
-    @Value("${telegram.bot.token}")
-    private String botToken;
     @Value("${telegram.chat.id}")
     private String chatId;
-    private WebClient webClient;
+    private final TelegramClient telegramClient;
 
-    @PostConstruct
-    private void init() {
-        this.webClient = WebClient.create("https://api.telegram.org/bot" + botToken);
+    public TelegramNotificationService(TelegramClient telegramClient) {
+        this.telegramClient = telegramClient;
     }
 
     @Override
     public void sendNotification(String message) {
-        webClient.post()
-                .uri(uriBuilder -> uriBuilder.path("/sendMessage")
-                        .queryParam("chat_id", chatId)
-                        .queryParam("text", message)
-                        .build())
-                .retrieve()
-                .bodyToMono(Void.class)
-                .block();
+        telegramClient.sendMessage(chatId, message);
     }
 }
